4e50b713e20ea2301db7730b548b4a3642de5c47
Shiven Khurana <shivenbk@gmail.com>
2026-07-11 12:46:07 -0400

    null loading

--- src/states/boot.js
+++ src/states/boot.js
@@ -6,7 +6,7 @@ class Boot extends Phaser.State {
   preload() {
     const browser = detect.detect();
     // In Firefox Linux, the right stick keys are different for some reason
-    if (browser.name === 'firefox' && browser.os === 'Linux') {
+    if (browser && browser.name === 'firefox' && browser.os === 'Linux') {
       Phaser.Gamepad.XBOX360_STICK_RIGHT_X = 3;
       Phaser.Gamepad.XBOX360_STICK_RIGHT_Y = 4;
       Phaser.Gamepad.XBOX360_START = 7;
